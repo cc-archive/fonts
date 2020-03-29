8b57a0211607deb27b43a29323c93562683f52a7
megha070 <varshney.megha070@gmail.com>
2020-03-29 18:38:04 +0530

    Replace JS with MDX Doc

---
+++ src/stories/story.js
@@ -0,0 +1,20 @@
+export const storyFn = () => `<style>
+.source-sans-pro {
+  font-family: 'Source Sans Pro';
+}
+.semi-bold {
+  font-weight: 600;
+}
+.bold {
+  font-weight: 700;
+}
+</style>
+<div class="source-sans-pro has-text-black has-bottom-margin">
+${storyFn()}
+</div>
+<div class="source-sans-pro semi-bold has-text-black has-bottom-margin">
+${storyFn()}
+</div>
+<div class="source-sans-pro bold has-text-black">
+${storyFn()}
+</div>`
