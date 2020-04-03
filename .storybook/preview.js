fc3ba88fb58f38643e0b6451628f90d92dc7b757
Dhruv Bhanushali <dhruv_b@live.com>
2020-04-03 12:11:47 +0530

    Update documentation

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,43 +1,39 @@
 import { addDecorator, addParameters, } from '@storybook/html'
 
 import '../dist/css/fonts.css'
+
 const order = {
-    'Fonts': [
-      'Introduction',
-      'Usage',
-      'Contribution'
-    ],
-    'Accidenz Commons': [],
-    'Roboto Condensed': [],
-    'Source Sans Pro': [],
-    'Vocabulary Icons': []
-  }
-  const families = Object.keys(order)
-  
-  addParameters({
-    options: {
-      showRoots: true,
-      storySort: ([, one], [, two]) => {
-        if (one.kind === two.kind) {
-          return 0 // Sort stories in a component as defined in the MDX file
-        }
-        const [famOne, componentOne] = one.kind.split('/')
-        const [famTwo, componentTwo] = two.kind.split('/')
-        if (famOne === famTwo) {
-          if (order[famOne].length) {
-            return order[famOne].indexOf(componentOne) - order[famOne].indexOf(componentTwo)
-          } else {
-            return componentOne.localeCompare(componentTwo) // Sort components in a family in alphabetical order
-          }
-        } else {
-          return families.indexOf(famOne) - families.indexOf(famTwo) // Sort families according to defined order
-        }
+  'Fonts': [
+    'Introduction',
+    'Usage',
+    'Contribution'
+  ],
+  'Vocabulary Icons': [],
+  'Roboto Condensed': [],
+  'Source Sans Pro': [],
+  'Accidenz Commons': [],
+}
+const families = Object.keys(order)
+
+addParameters({
+  options: {
+    showRoots: true,
+    storySort: ([, one], [, two]) => {
+      if (one.kind === two.kind) {
+        return 0 // Sort stories in a component as defined in the MDX file
       }
-    },
-    backgrounds: [
-      { name: 'canvas', value: '#f5f5f5', default: true },
-      { name: 'white', value: '#ffffff' },
-      { name: 'black', value: '#000000' }
-    ]
-  })
-  
+      const [famOne, componentOne] = one.kind.split('/')
+      const [famTwo, componentTwo] = two.kind.split('/')
+      if (famOne === famTwo) {
+        if (order[famOne].length) {
+          return order[famOne].indexOf(componentOne) - order[famOne].indexOf(componentTwo)
+        } else {
+          return componentOne.localeCompare(componentTwo) // Sort components in a family in alphabetical order
+        }
+      } else {
+        return families.indexOf(famOne) - families.indexOf(famTwo) // Sort families according to defined order
+      }
+    }
+  }
+})
+
